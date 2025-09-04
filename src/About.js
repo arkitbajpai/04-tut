edd88e5ad4638cc4daa772d69cba31edfe68d721
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-09-04 18:57:48 +0530

    Refactor About and Footer components: update structure, enhance styling, and improve content presentation

--- src/About.js
+++ src/About.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-   <main>
-    <h1>About</h1>
+   <main className='About'>
+    <h2>About</h2>
+   <p style={{marginTop:"1rem"}}>This is a React blog app built with React Router v6, 
+   demonstrating routing, state management, and CRUD operations.
+
+   </p>
    </main>
   )
 }
