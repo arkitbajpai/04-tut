7888ddcf11109720885162a2a41cf34242aaa3c5
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-09-05 15:19:12 +0530

    Add axios for API requests, implement post fetching, and create mock data

--- src/App.js
+++ src/App.js
@@ -10,39 +10,32 @@ import { Route, Routes, useNavigate } from 'react-router-dom';  // ✅ useNaviga
 import { useState, useEffect } from "react";
 import { format } from "date-fns";
 import './App.css';
+import api from "./api/post.js";
 
 function App() {
   const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    }
+    
   ]);
 
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+
+  useEffect(()=>
+  {
+    const fetchPosts = async () =>{
+      try{
+      const response= await api.get('/posts');
+      if(response && response.data){
+        setPosts(response.data);
+      }
+      
+    }
+    catch(err){console.log(err);}
+    }
+    fetchPosts();
+  },[])
   useEffect(()=>{
     const filteredResults = posts.filter((post)=>
       ((post.body).toLowerCase()).includes(search.toLowerCase()) ||
